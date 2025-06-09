e02967f03d4af55b72ee3d3c92a58753d2973821
Jordan Tsagmo <jordantsagmo@gmail.com>
2025-06-09 04:14:35 +0100

    login component ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import {Component} from '@angular/core';
+import {LoginComponent} from './component/login/login';
+import {Layout} from './component/layout/layout';
+import {Dashbord} from './component/dashboard/dashbord';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',redirectTo:'login',pathMatch:'full'
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'',
+    component:Layout,
+    children:[
+      {
+        path:'dashboard',
+        component:Dashbord
+      }
+    ]
+  }
+];
